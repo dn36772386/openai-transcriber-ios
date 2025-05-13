0ce04148a3c134282a409646a8a1da19cfbd0f24
dn36772386 <dn36772386@gmail.com>
2025-05-13 21:40:16 +0900

    Add initial Xcode project

---
+++ openai-transcriber-ios/openai_transcriber_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  openai_transcriber_iosApp.swift
+//  openai-transcriber-ios
+//
+//  Created by apple on 2025/05/13.
+//
+
+import SwiftUI
+
+@main
+struct openai_transcriber_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
