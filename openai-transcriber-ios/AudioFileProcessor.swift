adbd02b1668485a6c648198f24ae6b346de6eb1a
dn36772386 <dn36772386@gmail.com>
2025-05-24 23:41:57 +0900

    feat: AudioFileProcessorとAudioFormatExtensionの新規ファイルを追加

---
+++ openai-transcriber-ios/AudioFileProcessor.swift
@@ -0,0 +1,8 @@
+//
+//  AudioFileProcessor.swift
+//  openai-transcriber-ios
+//
+//  Created by apple on 2025/05/24.
+//
+
+import Foundation
---
+++ openai-transcriber-ios/AudioFormatExtension.swift
@@ -0,0 +1,8 @@
+//
+//  AudioFormatExtension.swift
+//  openai-transcriber-ios
+//
+//  Created by apple on 2025/05/24.
+//
+
+import Foundation
